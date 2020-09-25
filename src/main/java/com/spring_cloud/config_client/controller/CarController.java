18a52c3a074aef246a7774d1dfe5491c88f6a942
“Volodislav89” <shbloodislav@gmail.com>
2020-09-25 08:47:08 +0300

    Service and Controller reworked

--- src/main/java/com/spring_cloud/config_client/controller/CarController.java
+++ src/main/java/com/spring_cloud/config_client/controller/CarController.java
@@ -21,8 +21,23 @@ public class CarController {
         return carService.getAllCars();
     }
 
+    @GetMapping("/id/{id}")
+    public Car getCarById(@PathVariable Long id) {
+        return carService.getCarById(id);
+    }
+
     @PostMapping
     public Car saveNewCar(@RequestBody Car car) {
         return carService.saveNewCar(car);
+    }
+
+    @PutMapping("/update/{id}")
+    public Car updateCarById(@PathVariable Long id, @RequestBody Car car) {
+        return carService.updateCarById(id, car);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCarById(@PathVariable Long id) {
+        carService.deleteCarById(id);
     }
 }
--- src/main/java/com/spring_cloud/config_client/service/CarService.java
+++ src/main/java/com/spring_cloud/config_client/service/CarService.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -18,5 +19,22 @@ public class CarService {
 
     public Car saveNewCar(Car car) {
         return carRepository.save(car);
+    }
+
+    public Car getCarById(Long id) {
+        return carRepository.findById(id).get();
+    }
+
+    public Car updateCarById(Long id, Car car) {
+        Car newCar = carRepository.findById(id).get();
+        newCar.setBrand(car.getBrand());
+        newCar.setModel(car.getModel());
+        newCar.setColor(car.getColor());
+        newCar.setYear(car.getYear());
+        return carRepository.save(newCar);
+    }
+
+    public void deleteCarById(Long id) {
+        carRepository.deleteById(id);
     }
 }
